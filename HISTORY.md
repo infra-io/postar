ede1b75b7166ee343f079aadf22726e9171a6d71
FishGoddess <1149062639@qq.com>
2020-07-24 00:48:06 +0800

    更改开源协议为 MIT，完善英文文档

--- HISTORY.md
+++ HISTORY.md
@@ -1,6 +1,6 @@
 ## ✒ 历史版本的特性介绍 (Features in old versions)
 
 ### v0.1.0-alpha
-> 此版本发布于 2020-07-20
+> 此版本发布于 2020-07-24
 * 基础的 HTTP API 提供邮件发送功能
 * 支持 PLAIN 和 HTML 两种邮件格式
--- src/models/response.go
+++ src/models/response.go
@@ -1,16 +1,6 @@
-// Copyright 2020 Ye Zi Jie. All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Copyright 2020 Ye Zi Jie.  All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
 //
 // Author: FishGoddess
 // Email: fishgoddess@qq.com
@@ -20,20 +10,20 @@ package models
 
 import "encoding/json"
 
-// 响应结构体
+// response is the struct represents of the response of request.
 type response struct {
 
-	// code 是状态码
+	// code is the status code.
 	Code int `json:"code"`
 
-	// msg 是信息
+	// msg is the message.
 	Msg string `json:"msg"`
 
-	// data 是携带的数据
+	// data is the extra thing that returns.
 	Data interface{} `json:"data"`
 }
 
-// newResponse 返回一个完整的响应结构体对象
+// newResponse returns a response holder.
 func newResponse(code int, msg string, data interface{}) *response {
 	return &response{
 		Code: code,
@@ -44,47 +34,47 @@ func newResponse(code int, msg string, data interface{}) *response {
 
 // ======================================= all responses =======================================
 
-// 优化点：把这些常用的响应结果做成单例（sync.Once）甚至是提前序列化成 Json 的字节数组
-// 一开始考虑使用 sync.Once 做单例的方式，但是想想，每一个对象都要使用单独的 once 对象，
-// 这还是浪费了一些内存，所以最后选择一步到位，将常用的响应结果序列化成 Json 字节数组！
+// Optimization point: At first, we want to use singleton pattern to code these,
+// however, every responses will have a sync.Once holder which was originally a waste.
+// So we just serialize them in bytes and return bytes directly.
 var (
-	// 邮件成功发送的响应状态码和信息
+	// emailSuccessfullySentResponse means email successfully sent.
 	emailSuccessfullySentResponse, _ = json.Marshal(newResponse(0, "Email successfully sent!", nil))
 
-	// 服务器正在关闭的响应状态码和信息
+	// serverIsClosingResponse means server is closing.
 	serverIsClosingResponse, _ = json.Marshal(newResponse(0, "Server is closing! Please wait a few moments.", nil))
 
-	// 请求体错误的响应状态码和信息
+	// wrongRequestBodyResponse means you should check your request body.
 	wrongRequestBodyResponse, _ = json.Marshal(newResponse(100, "Please check your request body! It should be a Json string!", nil))
 
-	// 邮件发送失败的响应状态码和信息
+	// failedToSendEmailResponse means failed to send this email.
 	failedToSendEmailResponse, _ = json.Marshal(newResponse(200, "Failed to send this email, maybe the email has something wrong?", nil))
 
-	// 服务器关闭失败的响应状态码和信息
+	// failedToCloseServerResponse means failed to close server.
 	failedToCloseServerResponse, _ = json.Marshal(newResponse(300, "Failed to close server! Try to kill it?", nil))
 )
 
-// EmailSuccessfullySentResponse 返回一个邮件成功发送的响应结果
+// EmailSuccessfullySentResponse returns emailSuccessfullySentResponse.
 func EmailSuccessfullySentResponse() []byte {
 	return emailSuccessfullySentResponse
 }
 
-// ServerIsClosingResponse 返回一个服务器正在关闭的响应结果
+// ServerIsClosingResponse returns serverIsClosingResponse.
 func ServerIsClosingResponse() []byte {
 	return serverIsClosingResponse
 }
 
-// WrongRequestBodyResponse 返回一个请求体错误的响应结果
+// WrongRequestBodyResponse returns wrongRequestBodyResponse.
 func WrongRequestBodyResponse() []byte {
 	return wrongRequestBodyResponse
 }
 
-// FailedToSendEmailResponse 返回一个邮件发送失败的响应结果
+// FailedToSendEmailResponse returns failedToSendEmailResponse.
 func FailedToSendEmailResponse() []byte {
 	return failedToSendEmailResponse
 }
 
-// FailedToCloseServerResponse 返回一个服务器关闭失败的响应结果
+// FailedToCloseServerResponse returns failedToCloseServerResponse.
 func FailedToCloseServerResponse() []byte {
 	return failedToCloseServerResponse
 }
