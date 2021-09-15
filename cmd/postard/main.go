d7912fddef79b58cfd9aa54dac02bb2de545fa39
FishGoddess <1149062639@qq.com>
2021-09-16 02:06:42 +0800

    工程化实践重构

---
+++ cmd/postard/main.go
@@ -0,0 +1,13 @@
+// Copyright 2021 Ye Zi Jie.  All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+//
+// Author: FishGoddess
+// Email: fishgoddess@qq.com
+// Created at 2021/09/16 01:33:43
+
+package main
+
+func main() {
+
+}
---
+++ internal/postard/server/http.go
@@ -0,0 +1,9 @@
+// Copyright 2021 Ye Zi Jie.  All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+//
+// Author: FishGoddess
+// Email: fishgoddess@qq.com
+// Created at 2021/09/16 02:04:54
+
+package server
