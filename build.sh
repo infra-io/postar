643c944968ba69996d5e5fd7c36572da106805d7
FishGoddess <1149062639@qq.com>
2022-01-25 22:41:58 +0800

    修复 build.sh 打包问题

--- build.sh
+++ build.sh
@@ -7,12 +7,13 @@ echo "----------------------------------------------------------------------"
 # Check
 WORKDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 echo "WORKDIR: $WORKDIR"
+CONFIG_DIR=$WORKDIR/_examples/config
+CONFIG_FILE=postar.ini
+echo "CONFIG: $CONFIG_DIR/$CONFIG_FILE"
+LICENSE_FILE=LICENSE
+echo "LICENSE: $WORKDIR/$LICENSE"
 TARGET=$WORKDIR/target
 echo "TARGET: $TARGET"
-CONFIG_FILE=$WORKDIR/_examples/config/postar.ini
-echo "CONFIG_FILE: $CONFIG_FILE"
-LICENSE_FILE=$WORKDIR/LICENSE
-echo "LICENSE_FILE: $LICENSE_FILE"
 echo "----------------------------------------------------------------------"
 
 echo "Want to continue? (y/n)"
@@ -40,20 +41,20 @@ function build() {
   local BINARY_FILE=$3
   local PKG_FILE="$TARGET"/postar-$VERSION-$GOOS-$GOARCH.tar.gz
 
-  CGO_ENABLED=0 GOOS=$GOOS GOARCH=$GOARCH go build -o "$BINARY_FILE"
-  tar -czPf "$PKG_FILE" "$BINARY_FILE" "$CONFIG_FILE" "$LICENSE_FILE"
+  CGO_ENABLED=0 GOOS=$GOOS GOARCH=$GOARCH go build -o "$TARGET"/"$BINARY_FILE"
+  tar -czf "$PKG_FILE" -C "$TARGET" "$BINARY_FILE" -C "$CONFIG_DIR" "$CONFIG_FILE" -C "$WORKDIR" "$LICENSE_FILE"
   echo "The $GOOS-$GOARCH package can be found in $PKG_FILE"
-  rm "$BINARY_FILE"
+  rm "$TARGET"/"$BINARY_FILE"
 }
 
 echo "Building windows-amd64 version..."
-build windows amd64 "$TARGET"/postar.exe
+build windows amd64 postar.exe
 
 echo "Building linux-amd64 version..."
-build linux amd64 "$TARGET"/postar
+build linux amd64 postar
 
 echo "Building darwin-amd64 version..."
-build darwin amd64 "$TARGET"/postar
+build darwin amd64 postar
 
 # Done
 echo "Done."
